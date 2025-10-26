a210070cef556bb59b2591a94c42294ffacb6bad
Oleg Pentsov <OlegPensov@gmail.com>
2025-10-26 20:32:07 +0100

    added shadcn with menubar template, theme switcher implementation

---
+++ src/components/ui/button/Button.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { PrimitiveProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import type { ButtonVariants } from './index.ts'
+import { Primitive } from 'reka-ui'
+import { cn } from '@/lib/utils.ts'
+import { buttonVariants } from './index.ts'
+
+interface Props extends PrimitiveProps {
+  variant?: ButtonVariants['variant']
+  size?: ButtonVariants['size']
+  class?: HTMLAttributes['class']
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  as: 'button',
+})
+</script>
+
+<template>
+  <Primitive
+    :as="as"
+    :as-child="asChild"
+    :class="cn(buttonVariants({ variant, size }), props.class)"
+  >
+    <slot />
+  </Primitive>
+</template>
