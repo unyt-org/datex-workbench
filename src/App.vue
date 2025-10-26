dd616644871ead9df5a007ccde42432fe11d4313
Oleg Pentsov <OlegPensov@gmail.com>
2025-10-26 22:47:16 +0100

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,10 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import { Menubar, MenubarMenu } from '@/components/ui/menubar'
-import ThemeSwitch from '@/components/ThemeSwitch.vue'
+import { RouterView } from 'vue-router'
+import DatexWorkbenchHeaderComponent from '@/components/DatexWorkbenchHeaderComponent.vue'
 </script>
 
 <template>
-  <Menubar class="flex items-center w-full bg-background px-4 py-2 shadow-md">
-    <MenubarMenu class="flex items-center space-x-8">
-      <RouterLink to="/" class="text-foreground hover:text-primary transition-colors mr-4">
-        Home
-      </RouterLink>
-      <RouterLink to="/about" class="text-foreground hover:text-primary transition-colors mx-4">
-        About
-      </RouterLink>
-    </MenubarMenu>
-    <div class="ml-auto">
-      <ThemeSwitch />
-    </div>
-  </Menubar>
+  <DatexWorkbenchHeaderComponent />
 
   <RouterView />
 </template>
-
-<style scoped></style>
---
+++ src/components/DatexWorkbenchHeaderComponent.vue
@@ -0,0 +1,90 @@
+<script lang="ts">
+import ThemeSwitch from '@/components/ThemeSwitch.vue'
+import {
+  Menubar,
+  MenubarMenu,
+  MenubarTrigger,
+  MenubarSub,
+  MenubarItem,
+  MenubarSubTrigger,
+  MenubarSubContent,
+  MenubarContent,
+} from '@/components/ui/menubar/index.js'
+import IconLogo from '@/components/icons/IconLogo.vue'
+
+export default {
+  name: 'HeaderComponent',
+  components: {
+    MenubarSubContent,
+    MenubarSubTrigger,
+    MenubarContent,
+    MenubarItem,
+    MenubarSub,
+    IconLogo,
+    Menubar,
+    MenubarMenu,
+    MenubarTrigger,
+    ThemeSwitch,
+  },
+}
+</script>
+
+<template>
+  <Menubar class="flex items-center w-full bg-background px-4 py-2">
+    <RouterLink to="/" class="flex items-center space-x-2 group mr-4 border-primary transition-all">
+      <IconLogo class="w-8 h-8" />
+      <div class="flex items-baseline space-x-1">
+        <span class="text-lg font-bold text-foreground">DATEX Workbench</span>
+        <span class="text-xs unyt-blue font-semibold">beta</span>
+      </div>
+    </RouterLink>
+
+    <MenubarMenu class="flex items-center ml-8 space-x-4">
+      <MenubarTrigger>File</MenubarTrigger>
+      <MenubarContent>
+        <MenubarItem>New tab</MenubarItem>
+        <MenubarItem>New window</MenubarItem>
+        <MenubarSub>
+          <MenubarSubTrigger>Share</MenubarSubTrigger>
+          <MenubarSubContent>
+            <MenubarItem>Email</MenubarItem>
+            <MenubarItem>Link</MenubarItem>
+          </MenubarSubContent>
+        </MenubarSub>
+        <MenubarItem>Save</MenubarItem>
+        <MenubarItem>Save all</MenubarItem>
+        <MenubarItem>Close window</MenubarItem>
+        <MenubarItem>Close all</MenubarItem>
+        <MenubarItem>Preferences</MenubarItem>
+      </MenubarContent>
+    </MenubarMenu>
+    <MenubarMenu>
+      <MenubarTrigger>Edit</MenubarTrigger>
+      <MenubarContent>
+        <MenubarItem>Undo</MenubarItem>
+        <MenubarItem>Redo</MenubarItem>
+        <MenubarItem>Find</MenubarItem>
+        <MenubarItem>Cut</MenubarItem>
+        <MenubarItem>Copy</MenubarItem>
+        <MenubarItem>Paste</MenubarItem>
+      </MenubarContent>
+    </MenubarMenu>
+    <MenubarMenu>
+      <MenubarTrigger>View</MenubarTrigger>
+      <MenubarContent>
+        <MenubarItem>Reload</MenubarItem>
+        <MenubarItem>Toggle Fullscreen</MenubarItem>
+        <MenubarItem>Hide / Expand Sidebar</MenubarItem>
+      </MenubarContent>
+    </MenubarMenu>
+    <div class="ml-auto">
+      <ThemeSwitch />
+    </div>
+  </Menubar>
+</template>
+
+<style scoped>
+.unyt-blue {
+  color: rgb(42, 170, 215);
+}
+</style>
