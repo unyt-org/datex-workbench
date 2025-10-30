739bb8f453002d09f9de67b934a9561b8cb0add8
Lennart <lennart@unyt.org>
2025-10-30 11:55:55 +0100

    Intialized example DatexBlockProtocolViewer to understand project structure and router functionality

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,20 +3,24 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/home',
-            name: 'home',
-            component: HomeView,
-        },
-
-        {
-            path: '',
-            name: 'Editor',
-            component: WindowGeneralView,
-        },
-    ],
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/home',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/blocks',
+      name: 'blocks',
+      component: () => import('@/views/DatexBlockProtocolViewer.vue'),
+    },
+    {
+      path: '',
+      name: 'Editor',
+      component: WindowGeneralView,
+    },
+  ],
 });
 
 export default router;
---
+++ src/views/DatexBlockProtocolViewer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="BlockProtocolContainer">
+    <div class="RoutingHeaderContainer">
+      <p>Hier werden später die Datex Block Protocol Werte angezeigt</p>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+.BlockProtocolContainer {
+  padding: 3%;
+  background-color: var(--color-muted);
+}
+
+.RoutingHeaderContainer {
+  padding: 2%;
+  background-color: var(--color-card);
+  border-radius: 10pt;
+}
+</style>
