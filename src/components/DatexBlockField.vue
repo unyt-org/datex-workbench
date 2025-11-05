531fe0688344011d7a84d66ddde05c36fde525f1
Lennart <lennart@unyt.org>
2025-11-05 16:30:17 +0100

    not dynamic but statically working implementation to handle fields that habe subFields (e.g. Flags) to give different hover info or display hex numbers seperated

--- src/components/DatexBlockField.vue
+++ src/components/DatexBlockField.vue
@@ -13,26 +13,60 @@ function bufferToHex(buffer: ArrayBuffer) {
     .map((b) => b.toString(16).padStart(2, '0'))
     .join(' ')
 }
+
+const includesFlags = (str: string): boolean => str.includes("Flags");
+const hasParsedValue = (obj: object): boolean => 'parsedValue' in obj;
 </script>
 
 <template>
-  <div class="BlockProtocolField">
+  <div v-if="!field.subFields" class="BlockProtocolField">
     <Tooltip>
-      <TooltipTrigger>
-        <div class="FieldBytes">
+      <TooltipTrigger class="FieldBytes">
+        <div>
           {{ bufferToHex(field.bytes) }}
         </div>
       </TooltipTrigger>
       <TooltipContent>
         <div>{{ field.name }}</div>
-        <div>parsed Value: {{ field.parsedValue }}</div>
+        <div v-if="hasParsedValue(field)">Value: {{ field.parsedValue }}</div>
+      </TooltipContent>
+    </Tooltip>
+  </div>
+
+  <div v-else-if="includesFlags(field.name)" class="BlockProtocolField">
+    <Tooltip>
+      <TooltipTrigger class="FieldBytes">
+        <div>
+          {{ bufferToHex(field.bytes) }}
+        </div>
+      </TooltipTrigger>
+      <TooltipContent>
+        <div>{{ field.name }}</div>
+        <br />
+        <div v-for="(subField, index) in field.subFields[0]" :key="index">
+          <div>{{ subField.name }}</div>
+          <div v-if="hasParsedValue(subField)">Value: {{ subField.parsedValue }}</div>
+        </div>
+      </TooltipContent>
+    </Tooltip>
+  </div>
+
+  <div v-else class="BlockProtocolField" v-for="(subField, index) in field.subFields" :key="index">
+    <Tooltip>
+      <TooltipTrigger class="FieldBytes">
+        <div>
+          {{ bufferToHex(subField[0].bytes) }}
+        </div>
+      </TooltipTrigger>
+      <TooltipContent>
+        <div>{{ subField[0].name }}</div>
+        <div v-if="hasParsedValue(subField[0])">Value: {{ subField[0].parsedValue }}</div>
       </TooltipContent>
     </Tooltip>
   </div>
 </template>
 
 <style scoped>
-
 .FieldBytes {
   background-color: var(--color-chart-3);
   text-align: left;
