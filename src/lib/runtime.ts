338d6766a783931962a55cff0b7944ba72f5a17f
Benedikt Strehle <benedikt@unyt.org>
2025-11-06 00:08:02 +0100

    update datex runtime

--- src/lib/runtime.ts
+++ src/lib/runtime.ts
@@ -1,15 +1,33 @@
 // @ts-expect-error external import
-import { Datex } from "https://unyt-org.github.io/datex-core-js/datex.js";
-import type { Datex as _Datex } from "@unyt/datex";
+import { Runtime } from "https://unyt-org.github.io/datex-core-js/datex.js";
+import type { DIF } from "@unyt/datex";
+export type { DIF } from "@unyt/datex";
 
-export type DIFContainer = ReturnType<typeof _Datex.dif.executeSyncDIF>;
+/**
+ * The default configuration for the Datex runtime.
+ */
+const defaultConfig = {
+    // interfaces: [{
+    //     type: "websocket-client",
+    //     config: { address: "wss://example.unyt.land" },
+    // }],
+    debug: false, // set to true to show info/debug messages
+};
+
+/**
+ * The default instance of the Datex runtime.
+ */
+export const Datex: Runtime = await Runtime.create(defaultConfig, {
+    allow_unsigned_blocks: true,
+});
+
 
 /**
  * Returns a map of pointers that are currently loaded in the runtime.
  * The keys are pointer identifiers and the values are DIFContainer objects
  * representing the values stored at the pointer.
  */
-export function getPointers(): Map<string, DIFContainer> {
+export function getPointers(): Map<string, DIF.Definitions.DIFContainer> {
     // some example JS values
     const values = [
         42,
@@ -25,8 +43,8 @@ export function getPointers(): Map<string, DIFContainer> {
         }
     ]
     const pointers = new Map(values.map((value, index) => {
-        const difValue = Datex.dif.convertJSValueToDIFValue(value);
-        return [`$${index.toString().padStart(16, "0")}`, difValue] as [string, DIFContainer];
+        const difValue = Datex.dif.convertJSValueToDIFValue(value) as DIF.Definitions.DIFContainer;
+        return [`$${index.toString().padStart(16, "0")}`, difValue];
     }));
     return pointers;
 }
