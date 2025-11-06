86479d0b4d8e6522d87e9a8c4569812635b6e939
Oleg Pentsov <OlegPensov@gmail.com>
2025-11-06 03:03:47 +0100

    Window Manager is ready and documented

--- src/views/WindowGeneralView.vue
+++ src/views/WindowGeneralView.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="w-full h-full bg-primary-foreground text-primary p-1">
-    <Window :node="useLayoutTree().layoutTree"/>
+    <Window :node="useLayoutTree().layoutTree" />
   </div>
 </template>
 
 <script setup lang="ts">
 import Window from '@/components/Window.vue'
-import { useLayoutTree } from "@/composable/useLayoutTree.ts";
+import { useLayoutTree } from '@/composable/useLayoutTree.ts'
 </script>
 
 <style scoped></style>
