7cda23cd195bf47373aef6a6d5dc56e56a71bac8
Lennart <lennart@unyt.org>
2025-11-12 09:38:47 +0100

    actually fixed the category coloring issue and started field hovering feature

--- src/components/DatexBlockField.vue
+++ src/components/DatexBlockField.vue
@@ -9,25 +9,15 @@ const props = defineProps({
     type: Number,
     required: true,
   },
+  fieldColor: {
+    type: String,
+    required: false,
+  },
 });
 
 const bytesCutoff: number = 30;
 
 const uint8ToHexString = (b: number): string => b.toString(16).padStart(2, '0');
-
-const categories = [
-  ['Magic Number', 'Version', 'Context ID', 'Section Index', 'Block Number'],
-  ['Block Size', 'Flags', 'Flags and Timestamp', 'Lifetime'],
-  ['Checksum', 'Signature', 'Encrypted Signature', 'IV'],
-  ['Distance', 'TTL'],
-  ['Sender', 'Number of Receivers', 'Receivers', 'Represented By', 'On Behalf Of'],
-  ['Receivers Pointer ID', 'Receivers with Keys'],
-];
-
-function getCategoryColor(str: string) {
-  const index = categories.findIndex((subArray) => subArray.includes(str));
-  return `var(--color-chart-${index !== -1 ? (index % 5) + 1 : null}`;
-}
 </script>
 
 <template>
@@ -35,20 +25,16 @@ function getCategoryColor(str: string) {
     <div
       v-for="(byte, indexInner) in field.bytes.slice(0, bytesCutoff)"
       :key="indexInner"
-      :style="{ backgroundColor: getCategoryColor(field.name) }"
+      :style="{ backgroundColor: fieldColor }"
     >
       {{ uint8ToHexString(byte) }}
     </div>
-    <div
-      v-if="field.bytes.length > bytesCutoff"
-      :style="{ backgroundColor: getCategoryColor(field.name) }"
-    >
-      ..
-    </div>
+    <div v-if="field.bytes.length > bytesCutoff" :style="{ backgroundColor: fieldColor }">..</div>
   </div>
 </template>
 
-<style scoped>
+<style>
+
 .field-wrapper div {
   padding-left: 0.75ch;
 }
