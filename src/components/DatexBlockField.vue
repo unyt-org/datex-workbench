43bf25c351b274544bc495859f8eec6dfb443f08
Lennart <lennart@unyt.org>
2025-11-13 11:48:53 +0100

    solution idea for desired hover functionality in issue #42

--- src/components/DatexBlockField.vue
+++ src/components/DatexBlockField.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const props = defineProps({
   field: {
     type: Object,
@@ -20,17 +19,21 @@ const bytesCutoff: number = 25;
 const isExpanded = ref(false);
 
 const uint8ToHexString = (b: number): string => b.toString(16).padStart(2, '0');
+
+const emit = defineEmits(['field-clicked']);
+
+const handleClicks = (expand: boolean) => {
+  if(expand) {
+    isExpanded.value = !isExpanded.value
+  }
+  const data = structuredClone(props.field);
+  data.color = props.fieldColor;
+  emit('field-clicked', data);
+};
 </script>
 
 <template>
-  <div v-if="field.bytes.length <= bytesCutoff" class="field-wrapper contents">
-    <div v-for="(byte, indexInner) in field.bytes" :key="indexInner">
-      <div class="padding-wrapper" :style="{ backgroundColor: fieldColor }">
-        {{ uint8ToHexString(byte) }}
-      </div>
-    </div>
-  </div>
-  <div v-else class="field-wrapper contents cursor-pointer" @click="isExpanded = !isExpanded">
+  <div class="field-wrapper contents cursor-pointer" @click="handleClicks(true)">
     <div
       v-for="(byte, indexInner) in isExpanded ? field.bytes : field.bytes.slice(0, bytesCutoff)"
       :key="indexInner"
@@ -39,7 +42,7 @@ const uint8ToHexString = (b: number): string => b.toString(16).padStart(2, '0');
         {{ uint8ToHexString(byte) }}
       </div>
     </div>
-    <div v-if="!isExpanded">
+    <div v-if="!isExpanded && field.bytes.length > bytesCutoff">
       <div class="padding-wrapper" :style="{ backgroundColor: fieldColor }">..</div>
     </div>
   </div>
