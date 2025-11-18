10adc1e1756e318c64360c919b5f170044bb5eae
Lennart <lennart@unyt.org>
2025-11-18 18:11:52 +0100

    turned BlockInfo into shadcn table and defined upcoming features

--- src/components/DatexBlockInfo.vue
+++ src/components/DatexBlockInfo.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import Separator from '@/components/ui/separator/Separator.vue';
 import type { ParsedField } from '@unyt/speck';
+import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const props = defineProps<{
@@ -9,38 +10,92 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div class="info-grid grid gap-1">
+  <!-- The basic data like name, id, description and parsed Value with headers and as regular text
+  only the subfields as a table like I did but columns and rows reversed
+  incooperate category color in some way
+  -->
+  <h3>Version</h3>
+  <h4>id: </h4>
+  <p>description</p>
+  <!-- x-Button top right to get rid of Info box as well as clicking the field a second time also gets rid of info box -->
+
+  <!-- keep fields greyed out when viewing info in bottom view -->
+
+  <!-- add tooltip when hovering over a byte containing the name of the field and the
+   absolut byte offset of the whole block and where the field starts and ends as byte offset -->
+
+  <Table>
+    <TableBody>
+      <TableRow>
+        <TableCell class="w-[20%]">name</TableCell>
+        <TableCell class="w-[20%]"></TableCell>
+        <TableCell>{{ infoData.name }}</TableCell>
+      </TableRow>
+      <TableRow v-if="'id' in infoData">
+        <TableCell>id</TableCell>
+        <TableCell></TableCell>
+        <TableCell>{{ infoData.id }}</TableCell>
+      </TableRow>
+      <TableRow v-if="'parsedValue' in infoData">
+        <TableCell>parsed value</TableCell>
+        <TableCell></TableCell>
+        <TableCell>{{ infoData.parsedValue }}</TableCell>
+      </TableRow>
+      <div v-if="'subFields' in infoData" class="contents">
+        <div v-for="(subField, i) in infoData.subFields" :key="i" class="contents">
+          <TableRow></TableRow>
+          <TableRow>
+            <TableCell>subField {{ i + 1 }}</TableCell>
+            <TableCell>name</TableCell>
+            <TableCell>{{ subField.name }}</TableCell>
+          </TableRow>
+          <TableRow v-if="'id' in subField">
+            <TableCell></TableCell>
+            <TableCell>id</TableCell>
+            <TableCell>{{ subField.id }}</TableCell>
+          </TableRow>
+          <TableRow v-if="'parsedValue' in subField">
+            <TableCell></TableCell>
+            <TableCell>parsed value</TableCell>
+            <TableCell>{{ subField.parsedValue }}</TableCell>
+          </TableRow>
+        </div>
+      </div>
+    </TableBody>
+  </Table>
+
+  <!-- <div class="info-grid grid gap-1">
     <div class="contents">
-      <div class="col-span-2">Name</div>
+      <div class="col-span-2">name</div>
       <div class="break-all">{{ infoData.name }}</div>
     </div>
     <div v-if="'id' in infoData" class="contents">
       <Separator class="col-span-full" />
-      <div class="col-span-2">Id</div>
+      <div class="col-span-2">id</div>
       <div class="break-all">{{ infoData.id }}</div>
     </div>
     <div v-if="'parsedValue' in infoData" class="contents">
       <Separator class="col-span-full" />
-      <div class="col-span-2">Parsed Value</div>
+      <div class="col-span-2">parsed value</div>
       <div class="break-all">{{ infoData.parsedValue }}</div>
     </div>
     <div v-if="'subFields' in infoData" class="contents">
       <Separator class="col-span-full" />
-      <div>subFields</div>
+      <div>subfields</div>
       <div class="col-span-2 grid grid-cols-subgrid">
         <div v-for="(subField, i) in infoData.subFields" :key="i" class="contents">
           <div class="contents">
-            <div>Name</div>
+            <div>name</div>
             <div class="break-all">{{ subField.name }}</div>
             <Separator class="col-span-full" />
           </div>
           <div v-if="'id' in subField" class="contents">
-            <div>Id</div>
+            <div>id</div>
             <div class="break-all">{{ subField.id }}</div>
             <Separator class="col-span-full" />
           </div>
           <div v-if="'parsedValue' in subField" class="contents">
-            <div>Parsed Value</div>
+            <div>parsed Value</div>
             <div class="break-all">{{ subField.parsedValue }}</div>
             <Separator class="col-span-full" />
           </div>
@@ -48,7 +103,7 @@ const props = defineProps<{
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <style scoped>
--- src/views/DatexBlockProtocolViewer.vue
+++ src/views/DatexBlockProtocolViewer.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { parseStructure, type ParsedField } from '@unyt/speck';
+import { parseStructure, type ParsedField , type StructureDefinition} from '@unyt/speck';
 import DatexBlockSection from '@/components/DatexBlockSection.vue';
 import { ref } from 'vue';
 import {
@@ -11,7 +11,7 @@ import {
 import DatexBlockInfo from '@/components/DatexBlockInfo.vue';
 
 // this will later not be fetched from the example data
-const jsonDataExample = await (
+const jsonDataExample : StructureDefinition = await (
   await fetch(
     'https://raw.githubusercontent.com/unyt-org/datex-specification/refs/heads/main/assets/structures/dxb.json',
   )
@@ -24,6 +24,8 @@ const blockDataExample: Uint8Array = new Uint8Array(
   ).arrayBuffer(),
 );
 const structure = parseStructure(jsonDataExample, blockDataExample);
+// use this for more info ;)
+console.log(jsonDataExample)
 
 
 const clickedField = ref<ParsedField & { color: string }>();
@@ -37,15 +39,16 @@ const handleSectionFieldClick = (data: ParsedField & { color: string }) => {
   <div class="h-[calc(100%-15px)]">
     <Accordion
       type="multiple"
-      class="bg-background m-1 max-h-3/5 overflow-y-auto rounded-lg border p-[0.7rem]"
+      class="bg-background m-1 max-h-3/5 overflow-y-auto rounded-lg border px-4"
     >
       <AccordionItem
         v-for="(section, index) in structure"
         :key="index"
         :section="section"
         :value="`item-${index}`"
+        class="last:border-b-0"
       >
-        <AccordionTrigger class="text-foreground text-3xl">{{ section.name }}</AccordionTrigger>
+        <AccordionTrigger class="text-foreground text-md py-3 hover:no-underline cursor-pointer">{{ section.name }}</AccordionTrigger>
         <AccordionContent>
           <DatexBlockSection
             v-if="section.fields.length > 0"
@@ -57,7 +60,7 @@ const handleSectionFieldClick = (data: ParsedField & { color: string }) => {
     </Accordion>
     <div
       v-if="clickedField"
-      class="bg-background text-foreground m-1 max-h-2/5 overflow-y-auto rounded-lg border p-[0.7rem]"
+      class="bg-background text-foreground m-1 max-h-2/5 overflow-y-auto rounded-lg border px-4"
     >
       <DatexBlockInfo :infoData="clickedField"></DatexBlockInfo>
     </div>
