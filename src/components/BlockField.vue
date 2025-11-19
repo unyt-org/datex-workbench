87c607efa0ac7fe207addc11b023ce987317f389
Lennart <lennart@unyt.org>
2025-11-19 11:52:49 +0100

    beginning of restructuring of all files to improve the way I can handle data binding and states

---
+++ src/components/BlockField.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+import type { ParsedField } from '@unyt/speck';
+import { ref } from 'vue';
+
+const props = defineProps<{
+  field: ParsedField;
+  indexOuter: number;
+  fieldColor?: string;
+}>();
+
+const bytesCutoff: number = 25;
+const isExpanded = ref(false);
+
+const emit = defineEmits(['field-clicked']);
+
+const handleClick = () => {
+  let data: ParsedField & { color?: string } | undefined;
+  if(!isExpanded.value) {
+    data = structuredClone(props.field);
+    data.color = props.fieldColor;
+  } else {
+    data = undefined
+  }
+  isExpanded.value = !isExpanded.value;
+  emit('field-clicked', data);
+};
+
+const uint8ToHexString = (b: number): string => b.toString(16).padStart(2, '0');
+</script>
+
+<template>
+  <div class="field-wrapper contents cursor-pointer" @click="handleClick">
+    <div
+      v-for="(byte, indexInner) in isExpanded ? field.bytes : field.bytes.slice(0, bytesCutoff)"
+      :key="indexInner"
+    >
+      <div class="padding-wrapper leading-tight" :style="{ backgroundColor: fieldColor }">
+        {{ uint8ToHexString(byte) }}
+      </div>
+    </div>
+    <div v-if="!isExpanded && field.bytes.length > bytesCutoff">
+      <div class="padding-wrapper leading-tight" :style="{ backgroundColor: fieldColor }">..</div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.field-wrapper {
+  --total-column-width: 3ch;
+  --column-gap: 0.4ch;
+  --byte-field-radius: var(--radius-sm);
+
+  div {
+    padding: calc(var(--column-gap) / 2) 0ch;
+  }
+  div:first-child {
+    padding-left: calc(var(--column-gap) / 2);
+    .padding-wrapper {
+      padding-left: calc((var(--total-column-width) - 2ch - var(--column-gap)) / 2);
+      border-bottom-left-radius: var(--byte-field-radius);
+      border-top-left-radius: var(--byte-field-radius);
+    }
+  }
+  :not(div:first-child) {
+    .padding-wrapper {
+      padding-left: calc((var(--total-column-width) - 2ch) / 2);
+    }
+  }
+  div:last-child {
+    padding-right: calc(var(--column-gap) / 2);
+    .padding-wrapper {
+      padding-right: calc((var(--total-column-width) - 2ch - var(--column-gap)) / 2);
+      border-bottom-right-radius: var(--byte-field-radius);
+      border-top-right-radius: var(--byte-field-radius);
+    }
+  }
+  :not(div:last-child) {
+    .padding-wrapper {
+      padding-right: calc((var(--total-column-width) - 2ch) / 2);
+    }
+  }
+}
+</style>
---
+++ src/views/DatexBlockProtocolView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import BlockProtocolBytes from '@/views/BlockProtocolBytesView.vue';
+// import BlockProtocolInfo from '@/views/BlockProtocolInfoView.vue';
+import { parseStructure, type StructureDefinition } from '@unyt/speck';
+
+// this will later not be fetched from the example data
+const jsonDataExample: StructureDefinition = await (
+  await fetch(
+    'https://raw.githubusercontent.com/unyt-org/datex-specification/refs/heads/main/assets/structures/dxb.json',
+  )
+).json();
+const blockDataExample: Uint8Array = new Uint8Array(
+  await (
+    await fetch(
+      'https://raw.githubusercontent.com/unyt-org/datex-core/main/tests/structs/receivers_with_keys/block.bin',
+    )
+  ).arrayBuffer(),
+);
+const structure = parseStructure(jsonDataExample, blockDataExample);
+// use this for more info ;)
+// console.log(jsonDataExample);
+</script>
+
+<template>
+  <div class="h-[calc(100%-15px)]">
+    <div class="bg-background m-1 max-h-3/5 overflow-y-auto rounded-lg border px-4">
+      <BlockProtocolBytes :structure="structure"></BlockProtocolBytes>
+    </div>
+    <!-- <div class="bg-background m-1 max-h-3/5 overflow-y-auto rounded-lg border px-4">
+      <BlockProtocolInfo :infoData="undefined"></BlockProtocolInfo>
+    </div> -->
+  </div>
+</template>
