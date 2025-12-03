e007c4476e0d02686c778369c520858db7a4277b
Jonas Strehle <jonas@strehles.info>
2025-12-03 20:26:57 +0100

    View: Block protocol viewer (#27)
    Implemented block protocol viewer (#13)
    
    author: Lennart <lennart@unyt.org>

---
+++ src/components/BlockViewer/BlockFieldWrapper.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import type { FieldIdentifier } from '@/types/BlockViewer/FieldIdentifier';
+import type { FieldDefinition, ParsedField } from '@unyt/speck';
+import BlockField from './BlockField.vue';
+import { bytesCutoff } from '@/views/BlockViewer/settings';
+
+const props = defineProps<{
+    field: ParsedField;
+    fieldDef: FieldDefinition;
+    sectionId: number;
+    fieldId: number;
+    selectedField: FieldIdentifier | null;
+}>();
+
+const emit = defineEmits(['field-clicked']);
+function handleClick() {
+    emit(
+        'field-clicked',
+        fieldIsSelectedField()
+            ? null
+            : {
+                  sectionIndex: props.sectionId,
+                  fieldIndex: props.fieldId,
+              },
+    );
+}
+
+function fieldIsSelectedField() {
+    return (
+        props.sectionId === props.selectedField?.sectionIndex &&
+        props.fieldId === props.selectedField.fieldIndex
+    );
+}
+
+function cutFieldBytes(field: ParsedField): ParsedField {
+    const cutField = structuredClone(field);
+    cutField.bytes = cutField.bytes.slice(0, bytesCutoff);
+    return cutField;
+}
+
+const subfieldsMatchByteLength =
+    'subFields' in props.field &&
+    props.field.subFields.reduce((acc, subField) => acc + subField.bytes.length, 0) ===
+        props.field.bytes.length;
+</script>
+
+<template>
+    <div @click="handleClick" class="field-wrapper contents cursor-pointer">
+        <div v-if="!fieldIsSelectedField()" class="contents">
+            <BlockField
+                :field="cutFieldBytes(field)"
+                :shortenWithDots="field.bytes.length > bytesCutoff"
+                :fieldDef="fieldDef"
+            >
+            </BlockField>
+        </div>
+        <div v-else-if="!subfieldsMatchByteLength" class="contents">
+            <BlockField :field="field" :shortenWithDots="false" :fieldDef="fieldDef"></BlockField>
+        </div>
+        <div v-else class="subfield-wrapper contents">
+            <BlockField
+                v-for="(subField, index) in 'subFields' in field ? field.subFields : []"
+                :key="index"
+                :field="subField"
+                :shortenWithDots="false"
+                :fieldDef="fieldDef"
+            ></BlockField>
+        </div>
+    </div>
+</template>
---
+++ src/types/BlockViewer/FieldIdentifier.ts
@@ -0,0 +1,1 @@
+export type FieldIdentifier = { sectionIndex: number; fieldIndex: number };
