1066b68432a689060c560f81977864cada439fe6
Lennart <lennart@unyt.org>
2025-12-04 11:09:07 +0100

    feat: implemented unmovable cards to represent nodes

---
+++ src/components/NetworkTree/NetworkNode.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
+
+function mouseDown(event: MouseEvent) {
+    const card = event.currentTarget as HTMLElement;
+    card.classList.add('border-thick');
+}
+
+function mouseUp(event: MouseEvent) {
+    const card = event.currentTarget as HTMLElement;
+    card.classList.remove('border-thick');
+}
+</script>
+
+<template>
+    <Card
+        class="box-border w-xs border-2 border-transparent transition-all duration-100"
+        @mousedown.left="mouseDown"
+        @mouseup.left="mouseUp"
+        @mouseleave="mouseUp"
+    >
+        <CardHeader>
+            <CardTitle>Node</CardTitle>
+            <CardDescription>very pretty</CardDescription>
+            <CardAction>Card Action</CardAction>
+        </CardHeader>
+        <CardContent>
+            <p>some more data</p>
+        </CardContent>
+        <CardFooter>
+            <p>Card Footer</p>
+        </CardFooter>
+    </Card>
+</template>
+
+<style scoped>
+.border-thick {
+    border-color: inherit !important;
+}
+</style>
---
+++ src/views/TreeView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import NetworkNode from '@/components/NetworkTree/NetworkNode.vue';
+
+function mouseDown(event: MouseEvent) {
+    const card = event.currentTarget as HTMLElement;
+    console.log(`Move ${card.id}`);
+}
+
+function mouseUp(event: MouseEvent) {
+    const card = event.currentTarget as HTMLElement;
+    console.log(`Stop moving ${card.id}`);
+}
+</script>
+
+<template>
+    <div class="bg-primary-foreground text-primary h-full w-full p-1">
+        <NetworkNode
+            @mousedown.left="mouseDown"
+            @mouseup.left="mouseUp"
+            id="node1"
+            class="absolute"
+            style="top: 50px; left: 50px"
+        ></NetworkNode>
+        <NetworkNode
+            @mousedown.left="mouseDown"
+            @mouseup.left="mouseUp"
+            id="node2"
+            class="absolute"
+            style="top: 300px; left: 300px"
+        ></NetworkNode>
+    </div>
+</template>
