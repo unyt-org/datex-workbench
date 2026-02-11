f248537561e3da7f958ae908290fd65e8958603a
kha1dx <162775528+kha1dx@users.noreply.github.com>
2026-02-11 13:06:47 +0200

    install modern-monaco

---
+++ src/views/Editor/EditorViewWrapper.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="editor-wrapper">
+    <h1>Editor</h1>
+    <p>Editor component will be implemented here</p>
+
+  </div>
+</template>
+
+<style scoped>
+.editor-wrapper {
+  padding: 50px;
+}
+</style>
