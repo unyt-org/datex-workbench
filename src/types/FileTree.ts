fee67ba62d932d65f9e657a44e7ddfa184cbb235
Anmol Singh <theanmolvirk.11@gmail.com>
2026-03-18 11:11:45 +0100

    chore: remove accidentally committed ':' directory

--- src/types/FileTree.ts
+++
@@ -1,7 +0,0 @@
-export interface FileTreeNode {
-  name: string;
-  path: string;
-  type: 'file' | 'folder';
-  children?: FileTreeNode[];
-  isExpanded?: boolean;
-}
--- src/views/Editor/EditorViewWrapper.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import EditorView from './EditorView.vue';
-</script>
-
-<template>
-  <EditorView />
-</template>
