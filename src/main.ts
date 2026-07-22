4ab2369705545e78ce184953d3adfeb1039d3789
Benedikt Strehle <benedikt@unyt.org>
2026-07-22 23:47:54 +0200

    fix preferences

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { i18n } from './i18n';
+import { usePreferences } from '@/preferences';
+
+// Initialize preferences (theme + locale) on startup
+usePreferences();
 
 const app = createApp(App);
 app.use(router);
